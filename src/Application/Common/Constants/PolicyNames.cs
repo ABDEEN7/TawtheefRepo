66ee80d9712b351e747c7cf78c8437b6e603b75e
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2025-11-17 07:36:33 +0300

    Add new authorization policies: Admin, Employee, and Applicant

--- src/Application/Common/Constants/PolicyNames.cs
+++ src/Application/Common/Constants/PolicyNames.cs
@@ -3,4 +3,7 @@ namespace Tawtheef.Application.Common.Constants;
 public static class PolicyNames
 {
     public const string CompletedProfile = "CompletedProfile";
+    public const string Admin = "AdminOnly";
+    public const string Employee = "EmployeeOnly";
+    public const string Applicant = "ApplicantOnly";
 }
