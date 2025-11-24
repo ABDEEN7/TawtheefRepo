76340a2a5676dcf3db8ed7a1542d6fbc389ac5d6
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2025-11-24 09:41:28 +0300

    Annotate `BaseEntity` properties with `[Column(Order)]` to define precise column order in database schema.

--- src/Domain/Common/BaseEntity.cs
+++ src/Domain/Common/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using Tawtheef.Domain.Common.Interfaces;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Tawtheef.Domain.Common.Interfaces;
 using Tawtheef.Domain.Entities.Users;
 
 namespace Tawtheef.Domain.Common;
@@ -6,16 +7,27 @@ namespace Tawtheef.Domain.Common;
 public abstract class BaseEntity : IBaseEntity
 {
     public Guid Id { get; set; }
+    
+    [Column(Order = 93)]
     public Guid? CreatedById { get; set; }
     public User? CreatedBy { get; set; }
+    
+    [Column(Order = 94)]
     public DateTimeOffset CreatedDate { get; set; }
     
+    [Column(Order = 95)]
     public Guid? UpdatedById { get; set; }
     public User? UpdatedBy { get; set; }
+    
+    [Column(Order = 96)]
     public DateTimeOffset? UpdatedDate { get; set; }
+    
+    [Column(Order = 97)]
     public Guid? DeletedById { get; set; }
     public User? DeletedBy { get; set; }
-    public DateTimeOffset? DeletedDate { get; set; }
     
+    [Column(Order = 98)]
+    public DateTimeOffset? DeletedDate { get; set; }
+    [Column(Order = 99)]
     public bool IsDeleted { get; set; }
 }
