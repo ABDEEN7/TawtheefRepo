4da741ebcecdb00137fbe52e370a443541a0aef1
t-m.khatatbeh-dev <t-m.khatatbeh-dev@edu.gov.qa>
2025-11-27 11:30:39 +0300

    fixes

--- src/Infrastructure/Data/TawtheefDbContext.cs
+++ src/Infrastructure/Data/TawtheefDbContext.cs
@@ -89,6 +89,7 @@ public class TawtheefDbContext(DbContextOptions<TawtheefDbContext> options,
     // Recruitment Tables
     public DbSet<Job> Jobs { get; set; }
     public DbSet<Invitation> Invitations { get; set; }
+    public DbSet<JobCondition> JobConditions { get; set; }
     public DbSet<JobDegree> JobDegrees { get; set; }
     public DbSet<JobQuota> JobQuotas { get; set; }
     public DbSet<JobSkill> JobSkills { get; set; }
