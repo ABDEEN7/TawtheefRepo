11eab4044b5c3248563530a3697c118a6a62b726
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2025-12-13 02:15:47 +0300

    Add JWT authentication to employee-related controllers; secure access to job and profile distribution endpoints

--- src/Tawtheef.APIs/Operations.API/Controllers/Employee/ProfileApprovalsController.cs
+++ src/Tawtheef.APIs/Operations.API/Controllers/Employee/ProfileApprovalsController.cs
@@ -1,20 +1,25 @@
 using System.Security.Claims;
 using FluentResults;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Tawtheef.Application.Common.Constants;
 using Tawtheef.Application.Features.Operations.Employee.ProfileApprovals.Commands;
 using Tawtheef.Application.Features.Operations.Employee.ProfileApprovals.Queries;
 using Tawtheef.Application.Features.Recruitment.Profile.Command;
 using Tawtheef.Domain.Constants;
 using Tawtheef.Domain.Entities.Recruitment;
 using Tawtheef.Infrastructure.Extensions;
+using Tawtheef.Infrastructure.Services.Authorization;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Operations.API.Controllers.Employee;
 
 [ApiController]
 [Route("api/profile-approvals")]
-//[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+//[Authorize(Policy = PermissionPolicyProvider.POLICY_PREFIX + PermissionNames.JobsManage)]
 public class ProfileApprovalsController(IMediator mediator) : ControllerBase
 {
     private Result<Guid> UserId => User.FindFirst(ClaimTypes.NameIdentifier)?.Value switch
