86a542e3073b7434c26e4d4177133a5723c11654
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2025-12-25 09:29:33 +0300

    refactor: update LocalPathBuilder for recruitment and operation paths, and adjust storage settings

--- src/Application/Common/Services/LocalPathBuilder.cs
+++ src/Application/Common/Services/LocalPathBuilder.cs
@@ -2,26 +2,19 @@ namespace Tawtheef.Application.Common.Services;
 
 public static class LocalPathBuilder
 {
+    #region Recruitment
+    
     public static string UserProfile(Guid userId, string category, Guid fileId, string ext, string hash, bool isPublic)
-        => Key(Scope(isPublic), "u", userId.ToString(), "profile", category, FileName(fileId, hash, ext));
+        => Key(Scope(isPublic), "recruitment", "u", userId.ToString(), "profile", category, FileName(fileId, hash, ext));
 
-    public static string Ticket(Guid userId, Guid ticketId, Guid fileId, string ext, string hash, bool isPublic)
-        => Key(Scope(isPublic), "u", userId.ToString(), "tickets", ticketId.ToString(), FileName(fileId, hash, ext));
+    #endregion
 
-    public static string Invoice(Guid userId, Guid invoiceId, Guid fileId, string ext, string hash, bool isPublic)
-        => Key(Scope(isPublic), "u", userId.ToString(), "invoices", invoiceId.ToString(), FileName(fileId, hash, ext));
+    #region Operation
+    
+    public static string JobReview( Guid jobId, string tab, Guid fileId, string ext, string hash, bool isPublic)
+        => Key( Scope(isPublic), "operation", "j", jobId.ToString(), "reviews", tab, FileName(fileId, hash, ext));
 
-    public static string UniversityProfile(Guid uniId, Guid fileId, string ext, string hash, bool isPublic)
-        => Key(Scope(isPublic), "uni", uniId.ToString(), "profile", FileName(fileId, hash, ext));
-
-    public static string CourseCover(Guid courseId, Guid fileId, string ext, string hash, bool isPublic)
-        => Key(Scope(isPublic), "c", courseId.ToString(), "cover", FileName(fileId, hash, ext));
-
-    public static string LessonFile(Guid courseId, Guid versionLineId, Guid topicId, Guid lessonId,
-        Guid fileId, string ext, string hash, bool isPublic)
-        => Key(Scope(isPublic), "c", courseId.ToString(), "v", versionLineId.ToString(),
-            "topics", topicId.ToString(), "lessons", lessonId.ToString(),
-            "files", FileName(fileId, hash, ext));
+    #endregion
 
     private static string Scope(bool isPublic) => isPublic ? "public" : "private";
 
@@ -32,19 +25,4 @@ public static class LocalPathBuilder
     private static string Key(params string[] parts)
         => Path.Join(parts).Replace('\\', '/');
 
-    public static string JobReview(
-    Guid jobId,
-    string tab,
-    Guid fileId,
-    string ext,
-    string hash,
-    bool isPublic)
-    => Key(
-        Scope(isPublic),
-        "jobs",
-        jobId.ToString(),
-        "reviews",
-        tab,
-        FileName(fileId, hash, ext)
-    );
 }
