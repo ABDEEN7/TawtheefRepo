8ac9c64acdfee986d2ba847cef37aa3de653f4ad
Mahmoud Abdin Dev <t-m.abdin-dev@edu.gov.qa>
2025-12-28 11:05:29 +0300

    feat: update admin user IDs and comment out office import method for future reference

--- src/Domain/Entities/Users/AdminUser.cs
+++ src/Domain/Entities/Users/AdminUser.cs
@@ -1,10 +1,10 @@
 ﻿namespace Tawtheef.Domain.Entities.Users;
 public static class AdminUserIds
 {
-    public static readonly Guid Admin1UserId = new("a2c6d553-1c3d-4697-99d2-7e12f6335f38");
-    public static readonly Guid Admin2UserId = new("a2c6d553-1c3d-4697-99d2-7e12f6335f38");
-    public static readonly Guid Admin3UserId = new("a2c6d553-1c3d-4697-99d2-7e12f6335f38");
-    public static readonly Guid Admin4UserId = new("a2c6d553-1c3d-4697-99d2-7e12f6335f38");
+    public static readonly Guid Admin1UserId = new("26058720-5808-435a-abbf-d7a4e751f51e");
+    public static readonly Guid Admin2UserId = new("a8e0f354-2e23-41e1-9e1b-a1501b72dc4b");
+    public static readonly Guid Admin3UserId = new("48f6d7a2-e821-4ab6-81cf-884ed650d2ec");
+    public static readonly Guid Admin4UserId = new("5a4c8063-07a4-43ba-b4a8-8d980a323738");
 }
 public class AdminUser : User
 {
