46e1d5fdbc080c99e37c9324d270185d34e348b5
t-m.khatatbeh-dev <t-m.khatatbeh-dev@edu.gov.qa>
2025-12-29 14:43:00 +0300

    fixes

--- src/Tawtheef.Websites/operations-web/src/app/pages/job/modals/basics-step-modal/job-basic-modal.component.ts
+++ src/Tawtheef.Websites/operations-web/src/app/pages/job/modals/basics-step-modal/job-basic-modal.component.ts
@@ -195,7 +195,7 @@ export class JobBasicModalComponent implements OnInit, OnDestroy {
       subMajorId: formValue.subMajorId as GUID,
       workTypeId: formValue.workTypeId as GUID,
       numberOfVacancies: formValue.numberOfVacancies,
-      closingDate: formValue.closingDate ? new Date(formValue.closingDate) : new Date(),
+      closingDate: this.normalizeDate(formValue.closingDate),
       minimumAge: formValue.minimumAge,
       maximumAge: formValue.maximumAge,
       overviewAr: '',
@@ -321,10 +321,12 @@ export class JobBasicModalComponent implements OnInit, OnDestroy {
     return !!this.form.controls.majorId.value;
   }
 
-  private toDateOnly(d: Date): string {
-  const y = d.getFullYear();
-  const m = String(d.getMonth() + 1).padStart(2, '0');
-  const day = String(d.getDate()).padStart(2, '0');
-  return `${y}-${m}-${day}`; // YYYY-MM-DD
+  private normalizeDate(d: Date | null): Date {
+    if(!d)
+      return new Date();
+
+  const x = new Date(d);
+  x.setHours(12, 0, 0, 0); // noon local time
+  return x;
 }
 }
