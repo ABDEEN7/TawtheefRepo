41791a639bf523b519f356f0d0e1d1c4ed083e9a
Mahmoud Abdin Dev <t-m.abdin-dev@edu.gov.qa>
2025-12-29 12:21:22 +0300

    feat: add concurrency stamps to application roles for improved versioning

--- src/Infrastructure/Configurations/Entities/ApplicationRoleConfiguration.cs
+++ src/Infrastructure/Configurations/Entities/ApplicationRoleConfiguration.cs
@@ -20,7 +20,7 @@ public sealed class ApplicationRoleConfiguration
                 DescriptionAr = "مدير النظام الكامل",
                 DescriptionEn = "Full system administrator",
                 IsSystemRole = true,
-                
+                ConcurrencyStamp = "00ef28cd-ca1e-4a83-9a29-7e6e07c50e78"
             },
             new ApplicationRole
             {
@@ -31,7 +31,8 @@ public sealed class ApplicationRoleConfiguration
                 NameEn = "Employee",
                 DescriptionAr = "موظف اساسي",
                 DescriptionEn = "Basic Employee",
-                IsSystemRole = true
+                IsSystemRole = true,
+                ConcurrencyStamp = "82bb983f-1a91-4572-8d70-0598885b0514"
             },
             new ApplicationRole
             {
@@ -42,7 +43,8 @@ public sealed class ApplicationRoleConfiguration
                 NameEn = "Office Admin",
                 DescriptionAr = "مدير المكتب والصلاحيات المرتبطة",
                 DescriptionEn = "Office administrator",
-                IsSystemRole = true
+                IsSystemRole = true,
+                ConcurrencyStamp = "51961925-7d86-4e6a-bf60-646475d80319"
             },
             new ApplicationRole
             {
@@ -53,7 +55,8 @@ public sealed class ApplicationRoleConfiguration
                 NameEn = "Office User",
                 DescriptionAr = "موظف المكتب العادي",
                 DescriptionEn = "Office user",
-                IsSystemRole = true
+                IsSystemRole = true,
+                ConcurrencyStamp = "d0f6fe80-b684-40c0-9351-727e2feef829"
             },
             new ApplicationRole
             {
@@ -64,7 +67,8 @@ public sealed class ApplicationRoleConfiguration
                 NameEn = "Employee Super Admin",
                 DescriptionAr = "موظف بصلاحية كاملة",
                 DescriptionEn = "Employee Super Admin",
-                IsSystemRole = false
+                IsSystemRole = false,
+                ConcurrencyStamp = "b8d1862f-102c-4e76-9d53-3e9452930ce0"
             }
         );
     }
