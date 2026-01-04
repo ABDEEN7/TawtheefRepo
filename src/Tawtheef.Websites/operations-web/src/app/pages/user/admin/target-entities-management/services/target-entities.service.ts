c94760624418a78e1dfd38934548d8335722277b
Mahmoud Abdin Dev <t-m.abdin-dev@edu.gov.qa>
2026-01-04 13:46:31 +0300

    refactor: replace HttpClient with HttpService and update target entities retrieval

--- src/Tawtheef.Websites/operations-web/src/app/pages/user/admin/target-entities-management/services/target-entities.service.ts
+++ src/Tawtheef.Websites/operations-web/src/app/pages/user/admin/target-entities-management/services/target-entities.service.ts
@@ -1,17 +1,19 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import {PaginatedResult} from '../../../../../core/models/paginated-result.model';
 import {TargetEntityDto} from '../models/target-entity.dto';
 import {TargetEntityFilters} from '../models/target-entity-filters.dto';
 import {EndpointsService} from '../../../../../core/http/endpoints.service';
+import {Observable} from 'rxjs';
+import {CountryDto} from '../../countries-management/models/country.dto';
+import {HttpService} from '../../../../../core/http/http.service';
 
 @Injectable({providedIn: 'root'})
 export class TargetEntitiesService {
-  private http = inject(HttpClient);
+  private http = inject(HttpService);
   private endpoints = inject(EndpointsService);
 
-  getTargetEntities(filters: TargetEntityFilters) {
-    return this.http.get<PaginatedResult<TargetEntityDto>>(this.endpoints.targetEntities.listTargetEntities, filters);
+  getTargetEntities(filters: TargetEntityFilters): Observable<PaginatedResult<TargetEntityDto>> {
+    return this.http.get<PaginatedResult<CountryDto>>(this.endpoints.countries.listCountries, filters);
   }
 
   getTargetEntityDetails(id: string) {
