97c0a96095c6e6c66d30f9b0a7b7cae676b4aeed
HumaZamir <humaza@gmail.com>
2026-01-07 11:54:09 +0300

    UI fixes done

--- src/Tawtheef.Websites/operations-web/src/app/pages/user/employee/organization-structures/dialogs/upsert-management.dialog.ts
+++ src/Tawtheef.Websites/operations-web/src/app/pages/user/employee/organization-structures/dialogs/upsert-management.dialog.ts
@@ -34,82 +34,91 @@ export interface UpsertManagementDialogData {
     Select,
   ],
   template: `
-      <form (ngSubmit)="save()" #f="ngForm" class="d-flex flex-column gap-3">
-        <div class="flex-grow-1 min-w-250">
-          <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_SECTOR' | translate }}</label>
-          <p-select
-            class="w-100"
-            [options]="data.sectors"
-            optionLabel="name"
-            optionValue="id"
-            [(ngModel)]="vm.sectorId"
-            name="sectorId"
-            [showClear]="true"
-            [filter]="true"
-            [placeholder]="'ORG_STRUCTURES.FIELD_SECTOR' | translate">
-          </p-select>
-          <small class="text-muted" *ngIf="f.submitted && !vm.sectorId">{{ 'ORG_STRUCTURES.VALIDATION_REQUIRED' | translate }}</small>
+  <div class="modal-body">
+       <form (ngSubmit)="save()" #f="ngForm" class="d-flex flex-column gap-3">
+             <div class="row">
+                  <div class="col-md-12">
+                    <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_SECTOR' | translate }}</label>
+                    <p-select
+                      class="w-100"
+                      [options]="data.sectors"
+                      optionLabel="name"
+                      optionValue="id"
+                      [(ngModel)]="vm.sectorId"
+                      name="sectorId"
+                      [showClear]="true"
+                      [filter]="true"
+                      [placeholder]="'ORG_STRUCTURES.FIELD_SECTOR' | translate">
+                    </p-select>
+                    <small class="text-muted" *ngIf="f.submitted && !vm.sectorId">{{ 'ORG_STRUCTURES.VALIDATION_REQUIRED' | translate }}</small>
+                  </div>
+             </div>
+        
+
+        <div class="row">
+          <div class="col-md-6">
+              <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_NAME_EN' | translate }}</label>
+              <input pInputText class="w-100 form-control"
+                    name="nameEn"
+                    [(ngModel)]="vm.nameEn"
+                    required
+                    maxlength="200"
+                    [placeholder]="'ORG_STRUCTURES.FIELD_NAME_EN' | translate" />
+              <small class="text-muted" *ngIf="f.submitted && !vm.nameEn">{{ 'ORG_STRUCTURES.VALIDATION_REQUIRED' | translate }}</small>
+          </div>
+          <div class="col-md-6">
+              <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_NAME_AR' | translate }}</label>
+              <input pInputText class="w-100 form-control"
+                    name="nameAr"
+                    [(ngModel)]="vm.nameAr"
+                    required
+                    maxlength="200"
+                    [placeholder]="'ORG_STRUCTURES.FIELD_NAME_AR' | translate" />
+              <small class="text-muted" *ngIf="f.submitted && !vm.nameAr">{{ 'ORG_STRUCTURES.VALIDATION_REQUIRED' | translate }}</small>
+          </div>
         </div>
 
-        <div class="d-flex gap-3 flex-wrap">
-          <div class="flex-grow-1 min-w-250">
-            <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_NAME_EN' | translate }}</label>
-            <input pInputText class="w-100"
-                   name="nameEn"
-                   [(ngModel)]="vm.nameEn"
-                   required
-                   maxlength="200"
-                   [placeholder]="'ORG_STRUCTURES.FIELD_NAME_EN' | translate" />
-            <small class="text-muted" *ngIf="f.submitted && !vm.nameEn">{{ 'ORG_STRUCTURES.VALIDATION_REQUIRED' | translate }}</small>
-          </div>
-          <div class="flex-grow-1 min-w-250">
-            <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_NAME_AR' | translate }}</label>
-            <input pInputText class="w-100"
-                   name="nameAr"
-                   [(ngModel)]="vm.nameAr"
-                   required
-                   maxlength="200"
-                   [placeholder]="'ORG_STRUCTURES.FIELD_NAME_AR' | translate" />
-            <small class="text-muted" *ngIf="f.submitted && !vm.nameAr">{{ 'ORG_STRUCTURES.VALIDATION_REQUIRED' | translate }}</small>
-          </div>
-        </div>
-
-        <div class="d-flex gap-3 flex-wrap">
-          <div class="flex-grow-1 min-w-250">
-            <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_DESCRIPTION_EN' | translate }}</label>
-            <textarea pInputTextarea rows="3" class="w-100"
+        <div class="row">
+          <div class="col-md-6">
+              <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_DESCRIPTION_EN' | translate }}</label>
+              <textarea pInputTextarea rows="3" class="w-100 form-control"
                       name="descriptionEn"
                       [(ngModel)]="vm.descriptionEn"
                       maxlength="500"
                       [placeholder]="'ORG_STRUCTURES.FIELD_DESCRIPTION_EN' | translate"></textarea>
           </div>
 
-          <div class="flex-grow-1 min-w-250">
-            <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_DESCRIPTION_AR' | translate }}</label>
-            <textarea pInputTextarea rows="3" class="w-100"
-                      name="descriptionAr"
+          <div class="col-md-6">
+                  <label class="form-label">{{ 'ORG_STRUCTURES.FIELD_DESCRIPTION_AR' | translate }}</label>
+                  <textarea pInputTextarea rows="3" class="w-100 form-control"
+                            name="descriptionAr"
                       [(ngModel)]="vm.descriptionAr"
                       maxlength="500"
                       [placeholder]="'ORG_STRUCTURES.FIELD_DESCRIPTION_AR' | translate"></textarea>
           </div>
         </div>
 
-        <div class="d-flex gap-3 flex-wrap align-items-center">
-          <div class="d-flex align-items-center gap-2">
+        <div class="row">
+          <div class="col-md-12">
             <p-toggle-switch [(ngModel)]="vm.isActive" name="isActive"></p-toggle-switch>
             <span>{{ 'ORG_STRUCTURES.FIELD_ACTIVE' | translate }}</span>
           </div>
         </div>
 
-        <div class="d-flex justify-content-end gap-2 mt-2">
-          <button pButton type="button" class="p-button-outlined" (click)="cancel()">
-            {{ 'ORG_STRUCTURES.CANCEL' | translate }}
-          </button>
-          <button pButton type="submit" [disabled]="!isValid()">
-            {{ 'ORG_STRUCTURES.SAVE' | translate }}
-          </button>
+        
+        <div class="modal-footer justify-content-end gap-3 border-0">
+              <button pButton type="button" class="p-button-outlined mw-200 text-center d-flex text-primary fs-16 gap-1" (click)="cancel()">
+              <i class="hgi hgi-stroke hgi-cancel-01 text-primary fs-18"></i>
+              {{ 'ORG_STRUCTURES.CANCEL' | translate }}
+            </button>
+            <button pButton type="submit" class="btn btn-primary mw-200 d-flex gap-2 justify-content-center" [disabled]="!isValid()">
+              <i class="hgi hgi-stroke hgi-floppy-disk fs-18"></i>
+              {{ 'ORG_STRUCTURES.SAVE' | translate }}
+            </button>
         </div>
       </form>
+  </div>
+     
   `
 })
 export class UpsertManagementDialogComponent {
