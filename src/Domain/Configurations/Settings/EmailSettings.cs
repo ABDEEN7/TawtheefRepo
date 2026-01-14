158da8c4d2561ee0a8e3c71bf64d831e9023037c
t-m.khatatbeh-dev <t-m.khatatbeh-dev@edu.gov.qa>
2026-01-14 12:32:49 +0300

    hot fix

--- src/Domain/Configurations/Settings/EmailSettings.cs
+++ src/Domain/Configurations/Settings/EmailSettings.cs
@@ -8,8 +8,10 @@ public sealed class EmailSettings
     [Required]
     public required string SmtpHost { get; init; }
     public int SmtpPort { get; init; } = 587;
-    public string? EmailUser { get; init; }
-    public string? EmailPass { get; init; }
+    [Required]
+    public required string EmailUser { get; init; }
+    [Required]
+    public required string EmailPass { get; init; }
     public bool EnableSsl { get; init; } = true;
 
     public string ProductName { get; init; } = "Tawtheef";
@@ -18,8 +20,10 @@ public sealed class EmailSettings
     public string? UnsubscribeHttpUrl { get; init; } // e.g. https://.../unsubscribe
     public string? UnsubscribeMailto { get; init; }  // e.g. mailto:unsubscribe@yourdomain
     public bool UseBccForMultiRecipient { get; init; } = true;
-    public  string? ManagerEmails { get; init; }
-    public string? ContactUsEmail { get; init; }
+    [Required]
+    public required string ManagerEmails { get; init; }
+    [Required]
+    public required string ContactUsEmail { get; init; }
     public string? LogoPath { get; init; }
     public int MaxSmtpClients { get; init; } = 3;
 }
