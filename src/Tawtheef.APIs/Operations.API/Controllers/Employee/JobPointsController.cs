f21d1b72aee576487f67338f2767b41a9274f927
t-m.khatatbeh-dev <t-m.khatatbeh-dev@edu.gov.qa>
2026-01-15 10:29:21 +0300

    Add Aprove Points Permissions

--- src/Tawtheef.APIs/Operations.API/Controllers/Employee/JobPointsController.cs
+++ src/Tawtheef.APIs/Operations.API/Controllers/Employee/JobPointsController.cs
@@ -15,7 +15,7 @@ namespace Operations.API.Controllers.Employee;
 public class JobPointsController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
-    [AuthorizePermission(PermissionKeys.Jobs.Manage)]
+    [AuthorizePermission(PermissionKeys.JobsPoints.Manage)]
     public async Task<IActionResult> AddJobPoints([FromBody] SaveJobPointsCommand command)
     {
         var result = await mediator.Send(command);
@@ -23,7 +23,7 @@ public class JobPointsController(IMediator mediator) : ControllerBase
     }
 
     [HttpGet("{jobId:guid}")]
-    [AuthorizePermission(PermissionKeys.Jobs.View, PermissionKeys.Jobs.Manage)]
+    [AuthorizePermission(PermissionKeys.JobsPoints.View, PermissionKeys.JobsPoints.Manage)]
     public async Task<IActionResult> GetJobPoints(Guid jobId)
     {
         var result = await mediator.Send(new GetJobPointsByJobIdQuery(jobId));
@@ -31,7 +31,7 @@ public class JobPointsController(IMediator mediator) : ControllerBase
     }
 
     [HttpPost("{jobId:guid}/approve")]
-    [AuthorizePermission(PermissionKeys.Jobs.Manage)]
+    [AuthorizePermission(PermissionKeys.JobsPoints.Approve)]
     public async Task<IActionResult> ApproveJobPoints(Guid jobId)
     {
         var result = await mediator.Send(new ApproveJobPointsCommand(jobId));
@@ -41,7 +41,7 @@ public class JobPointsController(IMediator mediator) : ControllerBase
     #region Points Configurations
 
     [HttpGet("config")]
-    [AuthorizePermission(PermissionKeys.Jobs.View, PermissionKeys.Jobs.Manage)]
+    [AuthorizePermission(PermissionKeys.JobsPoints.View, PermissionKeys.JobsPoints.Manage)]
     public async Task<IActionResult> GetJobPointsConfigurations()
     {
         var result = await mediator.Send(new GetJobPointsConfigurationsQuery());
@@ -49,7 +49,7 @@ public class JobPointsController(IMediator mediator) : ControllerBase
     }
 
     [HttpPost("configurations/save")]
-    [AuthorizePermission(PermissionKeys.Jobs.Manage)]
+    [AuthorizePermission(PermissionKeys.JobsPoints.Manage)]
     public async Task<IActionResult> SaveJobPointsConfiguration([FromBody] SaveJobPointsConfigurationCommand command)
     {
         var result = await mediator.Send(command);
