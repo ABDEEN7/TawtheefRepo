32b03566a68174b43fa28618fad0b65d7f093300
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2026-01-22 10:37:13 +0300

    refactor: clean up mapping-tab.html and remote-select.ts for improved readability

--- src/Tawtheef.Websites/operations-web/src/environments/environment.stage.ts
+++ src/Tawtheef.Websites/operations-web/src/environments/environment.stage.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://app-tawtheef-operations-api-dev-01-frcpgze6cva6g7bs.qatarcentral-01.azurewebsites.net/api'
+  apiBaseUrl: 'https://careers-admin-api.edu.gov.qa/api'
 };
--- src/Tawtheef.Websites/recruitment-web/src/environments/environment.stage.ts
+++ src/Tawtheef.Websites/recruitment-web/src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://app-tawtheef-recruitment-api-dev-01-ewg0dsfcdvcffvh4.qatarcentral-01.azurewebsites.net/api',
+  apiBaseUrl: 'https://careers-api.edu.gov.qa/api',
   qatarPassLoginUrl: 'https://moeheqpservice-stage.edu.gov.qa/home/index?ApplicationID=E1F73743-66B5-F011-9D08-0050569F0091'
 };
