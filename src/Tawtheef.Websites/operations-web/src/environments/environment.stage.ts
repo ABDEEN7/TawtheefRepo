635e95104134b893bb365411a2a97a308ef1ff3e
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2026-01-22 10:20:31 +0300

    refactor: update environments, titles, budgets, and build scripts; remove unused import in mapping-tab component

--- src/Tawtheef.Websites/operations-web/src/environments/environment.stage.ts
+++ src/Tawtheef.Websites/operations-web/src/environments/environment.stage.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.dev.example.com'
+  apiBaseUrl: 'https://app-tawtheef-operations-api-dev-01-frcpgze6cva6g7bs.qatarcentral-01.azurewebsites.net/api'
 };
--- src/Tawtheef.Websites/recruitment-web/src/environments/environment.stage.ts
+++ src/Tawtheef.Websites/recruitment-web/src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiBaseUrl: 'https://api.dev.example.com',
-  qatarPassLoginUrl: 'https://moeheqpservice-stage.edu.gov.qa/home/index?ApplicationID=E1F73743-66B5-F011-9D08-0050569F0091',
+  production: false,
+  apiBaseUrl: 'https://app-tawtheef-recruitment-api-dev-01-ewg0dsfcdvcffvh4.qatarcentral-01.azurewebsites.net/api',
+  qatarPassLoginUrl: 'https://moeheqpservice-stage.edu.gov.qa/home/index?ApplicationID=E1F73743-66B5-F011-9D08-0050569F0091'
 };
