668fd15dfdb9273578c8cf06b32901f9143e1669
t-m.khatatbeh-dev <t-m.khatatbeh-dev@edu.gov.qa>
2026-01-22 11:30:16 +0300

    Fixes

--- src/Application.Operation/Features/Employee/JobManagement/JobCandidates/DTOs/JobCandidateProfileDto.cs
+++ src/Application.Operation/Features/Employee/JobManagement/JobCandidates/DTOs/JobCandidateProfileDto.cs
@@ -1,17 +1,4 @@
 namespace Application.Operation.Features.Employee.JobManagement.JobCandidates.DTOs;
-
-public sealed record JobCandidatePointsBreakdownDto
-{
-    public int CategoryPoints { get; init; }
-    public int EducationPoints { get; init; }
-    public int ExperiencePoints { get; init; }
-    public int TrainingPoints { get; init; }
-    public int SkillPoints { get; init; }
-    public int LanguagePoints { get; init; }
-    public int CertificatePoints { get; init; }
-    public int TotalPoints { get; init; }
-}
-
 public sealed record JobCandidateProfileDto
 {
     public Guid CandidateId { get; init; }
