e1797c1704bb5596bfaec2fee46d0633089c746c
Mahmoud Abdin Dev <t-m.abdin-dev@edu.gov.qa>
2026-03-02 10:38:05 +0300

    feat: add ActionLog table for tracking user actions

---
+++ src/Infrastructure/Migrations/20260226114525_AddGeneralLogTable.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tawtheef.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGeneralLogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActionLog",
+                schema: "hr",
+                columns: table => new
+                {
+                    CreatedById = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedById = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedById = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ActionType = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    LogType = table.Column<int>(type: "int", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: true),
+                    Section = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    EntityId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    AttachmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActionLog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActionLog_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ActionLog_AspNetUsers_DeletedById",
+                        column: x => x.DeletedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ActionLog_AspNetUsers_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActionLog_CreatedById",
+                schema: "hr",
+                table: "ActionLog",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActionLog_DeletedById",
+                schema: "hr",
+                table: "ActionLog",
+                column: "DeletedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActionLog_UpdatedById",
+                schema: "hr",
+                table: "ActionLog",
+                column: "UpdatedById");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActionLog",
+                schema: "hr");
+        }
+    }
+}
