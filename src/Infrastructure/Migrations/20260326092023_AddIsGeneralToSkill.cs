f13b2714917377e1700138b0036cc4e9c1ebc884
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2026-03-26 14:23:37 +0300

    feat: make MajorId nullable in Job table and add IsGeneral column to Skill.

---
+++ src/Infrastructure/Migrations/20260326092023_AddIsGeneralToSkill.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tawtheef.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsGeneralToSkill : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGeneral",
+                schema: "lkp",
+                table: "Skill",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsGeneral",
+                schema: "lkp",
+                table: "Skill");
+        }
+    }
+}
