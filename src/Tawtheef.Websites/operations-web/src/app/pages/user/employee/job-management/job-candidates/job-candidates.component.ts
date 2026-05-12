49c597d134d9127642d38d94b127385a2e6c855a
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2026-05-12 12:40:33 +0300

    fix issue

--- src/Tawtheef.Websites/operations-web/src/app/pages/user/employee/job-management/job-candidates/job-candidates.component.ts
+++ src/Tawtheef.Websites/operations-web/src/app/pages/user/employee/job-management/job-candidates/job-candidates.component.ts
@@ -232,7 +232,7 @@ export class JobCandidatesComponent implements OnInit {
   }
 
   exportToExcel() {
-    if (!this.canManageJobs()) return;
+    if (!this.canSendInvitation()) return;
     const invitationIds =
       this.selectedCandidates.length > 0
         ? this.selectedCandidates.map((candidate) => candidate.candidateId)
@@ -256,7 +256,7 @@ export class JobCandidatesComponent implements OnInit {
   }
 
   sendInvitations() {
-    if (!this.canManageJobs()) return;
+    if (!this.canSendInvitation()) return;
     const ref = this.dialogHelperService.openConfirmDialog({
       type: 'submit',
       title: 'JOB_CANDIDATE_CONFIRMATIONS_SEND_INVITATIONS_TITLE',
