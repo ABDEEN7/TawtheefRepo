2849c5430ff457cd61284399919e534fad8aea69
Ala Jaber Dev <t-a.jaber-dev@edu.gov.qa>
2026-07-21 13:21:05 +0300

    fix name privcy and terms

--- src/Tawtheef.Websites/operations-web/src/environments/environment.ts
+++ src/Tawtheef.Websites/operations-web/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   storageProvider: 'Local' as 'Local' | 'Azure',
-  apiBaseUrl: 'https://localhost:7072/api'
+  apiBaseUrl: 'https://localhost:7072/api',
+  policyBaseUrl: 'https://www.edu.gov.qa/ar/?file=96ab4af5-88a1-4106-aa11-0643bd14926d'
 };
