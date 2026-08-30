b45828f910f043acd778dce3868fc3b89e2dcfc3
Mohammad Khatatbeh <t-m.khatatbeh-dev@edu.gov.qa>
2026-08-30 09:05:16 +0300

    fixes

--- src/Infrastructure/Migrations/TawtheefDbContextModelSnapshot.cs
+++ src/Infrastructure/Migrations/TawtheefDbContextModelSnapshot.cs
@@ -10121,10 +10121,6 @@ namespace Tawtheef.Infrastructure.Migrations
 
                     b.HasIndex("IsDeleted");
 
-                    b.HasIndex("Qid")
-                        .IsUnique()
-                        .HasFilter("[Qid] IS NOT NULL AND [Qid] <> N''");
-
                     b.HasIndex("UpdatedById");
 
                     b.ToTable("EmployeeProfile");
