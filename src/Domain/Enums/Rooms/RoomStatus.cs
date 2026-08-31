b2dc434c4e74e1409cca35ac37bf99c88acc3495
yazan <yazan@test.com>
2026-08-31 10:31:30 +0300

    fix

--- src/Domain/Enums/Rooms/RoomStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace Tawtheef.Domain.Enums.Rooms;
-
-public enum RoomStatus
-{
-    Available,
-    Unavailable,
-    UnderMaintenance
-}
--- src/Domain/Enums/Rooms/RoomType.cs
+++
@@ -1,9 +0,0 @@
-namespace Tawtheef.Domain.Enums.Rooms;
-
-public enum RoomType
-{
-    ExamRoom,
-    InterviewRoom,
-    LabRoom,
-    Other
-}
