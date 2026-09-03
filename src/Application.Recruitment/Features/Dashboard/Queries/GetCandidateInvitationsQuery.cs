2b87a8b1da58a6d8219534eca236feb05aacdda7
Mohammad Khatatbeh <t-m.khatatbeh-dev@edu.gov.qa>
2026-09-03 10:17:39 +0300

    order invitations descending by creation date

--- src/Application.Recruitment/Features/Dashboard/Queries/GetCandidateInvitationsQuery.cs
+++ src/Application.Recruitment/Features/Dashboard/Queries/GetCandidateInvitationsQuery.cs
@@ -5,6 +5,10 @@ using Tawtheef.Application.Common.Models.Pagination;
 
 namespace Application.Recruitment.Features.Dashboard.Queries;
 
-public sealed record GetCandidateInvitationsQuery(Guid? UserId, Guid? InvitationStatusId,Guid? JobCategoryId,Guid? DepartmentId,string? JobTitle)
+public sealed record GetCandidateInvitationsQuery(
+    Guid? UserId,
+    Guid? InvitationStatusId,
+    Guid? JobCategoryId,
+    Guid? DepartmentId,
+    string? JobTitle)
     : PaginatedRequest, IRequest<IResult<PaginatedResult<CandidateInvitationsDto>>>;
-
--- src/Tawtheef.Websites/recruitment-web/src/app/pages/user/dashboard/dashboard.ts
+++ src/Tawtheef.Websites/recruitment-web/src/app/pages/user/dashboard/dashboard.ts
@@ -80,7 +80,7 @@ export class Dashboard implements OnInit {
       pageNumber: this.currentPage(),
       pageSize: this.itemsPerPage(),
       sortBy: 'CreatedDate',
-      sortDirection: 'asc'
+      sortDirection: 'desc'
     }
 
     this.candidateService.loadCandidateInvitations(searchFilters);
