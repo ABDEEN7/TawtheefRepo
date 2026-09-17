34398e50f5d523df6ae1611cf9482d0d549772ae
Abd Alhafiz Mohammed <t-abd.mohammed@edu.gov.qa>
2026-09-17 10:51:44 +0300

    Add lookupDto when initialize TemplateForm

--- src/Tawtheef.APIs/Operations.API/Controllers/Interview/InterviewEvaluationTemplateController.cs
+++ src/Tawtheef.APIs/Operations.API/Controllers/Interview/InterviewEvaluationTemplateController.cs
@@ -13,6 +13,15 @@ namespace Operations.API.Controllers.Interview;
 [Microsoft.AspNetCore.Authorization.Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 public class InterviewEvaluationTemplateController(IMediator mediator) : ControllerBase
 {
+    // ---- Lookups ----
+
+    [HttpGet("lookups")]
+    [AuthorizePermission(PermissionKeys.InterviewEvaluationTemplate.View, PermissionKeys.InterviewEvaluationTemplate.Manage)]
+    public async Task<IActionResult> ListTemplateLookups(CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(new ListTemplateLookupsQuery(), cancellationToken);
+        return result.ToActionResult();
+    }
     // ---- Templates ----
 
     [HttpGet("templates")]
