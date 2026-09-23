03c74affc3a928638d43fe5342616f4e4ea997cb
Mahmoud Abdin Dev <t-m.abdin-dev@edu.gov.qa>
2026-09-23 13:35:14 +0300

    feat(migration): add QuestionBankEntry migration with role claims and permissions

---
+++ src/Infrastructure/Migrations/20260923081043_QuestionBankEntry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Tawtheef.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuestionBankEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoleClaims",
+                columns: new[] { "Id", "ClaimType", "ClaimValue", "RoleId" },
+                values: new object[,]
+                {
+                    { -1873868507, "permission", "question-bank-assignments.manage", new Guid("5b757ede-93e1-4c7a-88d3-5e89007d648b") },
+                    { -1769273956, "permission", "question-bank-assignments.manage", new Guid("5f12e420-f666-4af4-a8fa-4e4aa755fdcd") }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "lkp",
+                table: "Permission",
+                columns: new[] { "Id", "BackendName", "CreatedById", "CreatedDate", "DeletedById", "DeletedDate", "DescriptionAr", "DescriptionEn", "DisplayOrder", "IsActive", "IsAssignableToRole", "IsDeleted", "NameAr", "NameEn", "UpdatedById", "UpdatedDate" },
+                values: new object[] { new Guid("0e00482c-788c-ef59-ac2b-8404b84e2bf3"), "question-bank-assignments.manage", null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, null, null, null, 88, true, true, false, "مهام بنوك الأسئلة - إدارة", "Question Bank Assignments - Manage", null, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoleClaims",
+                keyColumn: "Id",
+                keyValue: -1873868507);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoleClaims",
+                keyColumn: "Id",
+                keyValue: -1769273956);
+
+            migrationBuilder.DeleteData(
+                schema: "lkp",
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: new Guid("0e00482c-788c-ef59-ac2b-8404b84e2bf3"));
+        }
+    }
+}
